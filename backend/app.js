acd7db776329b48231f61dafa0f26e980af8049d
Muhammad Salif <muhammad.salif@yahoo.com>
2020-11-21 23:59:55 +0500

    HardCoded client implemented & Server issues fixed

--- backend/app.js
+++ backend/app.js
@@ -12,58 +12,69 @@ app.options('*', cors());
 //body parse middleware
 app.use(express.json({ limit: '10kb' }));
 
-app.get('/api/getAll',async (req,res)=>{     
-  try{
+app.get('/api/getAll', async (req, res) => {
+  try {
     const doc = await Marksheet.find();
     return res.status(200).json({
-        status: 'success',
-        result: doc.length,
-        data: {doc}
-    })
-  }  
-  catch(err){
-    return   res.status(500).json({
+      status: 'success',
+      result: doc.length,
+      data: { doc },
+    });
+  } catch (err) {
+    return res.status(500).json({
       status: 'fail',
-      message: err.message
+      message: err.message,
     });
   }
-
-})
-
-app.get('/api/getOne',async (req,res)=>{
-      try{
-            let doc= await Marksheet.findById(req.params.id)
-            if(!doc) return res.status(404).json({
-              status: 'fail',
-              message: "Could not found record with id "+req.params.id
-            });
-            
-            res.status(200).json({
-                status: 'success',
-                data: {doc}
-            })
-      }  
-      catch(err){
-        return   res.status(500).json({
-          status: 'fail',
-          message: err.message
-        });
-      }
 });
 
-app.post('/api/calculate', async function (req,res) {
-  try{
-    const subjects =  req.body.subjects;
-    if(subjects != null || !Array.isArray(subjects) || subjects.length ===0){
-      return   res.status(500).json({
+app.get('/api/getOne', async (req, res) => {
+  try {
+    let doc = await Marksheet.findById(req.params.id);
+    if (!doc)
+      return res.status(404).json({
         status: 'fail',
-        message:"Please Provide All Required fields"
+        message: 'Could not found record with id ' + req.params.id,
+      });
+
+    res.status(200).json({
+      status: 'success',
+      data: { doc },
+    });
+  } catch (err) {
+    return res.status(500).json({
+      status: 'fail',
+      message: err.message,
+    });
+  }
+});
+
+app.post('/api/calculate', async function (req, res) {
+  try {
+    const subjects = req.body.subjects;
+    if (
+      subjects === null ||
+      !Array.isArray(subjects) ||
+      subjects.length === 0
+    ) {
+      return res.status(500).json({
+        status: 'fail',
+        message: 'Please Provide All Required fields',
       });
     }
-    if(subjects.Any(x=> !x.name || !x.obtainedMarks && x.name == null || x.obtainedMarks == null )){
-      return   res.status(500).json({
+
+    if (
+      subjects.some(
+        (x) =>
+          !x.name ||
+          !x.obtainedMarks ||
+          x.obtainedMarks == null ||
+          x.name == null
+      )
+    ) {
+      return res.status(500).json({
         status: 'fail',
-        message:"Please Provide All Required fields"
+        message: 'Please Provide All Required fields',
       });
     }
     let totalMarks = 0;
@@ -71,45 +82,43 @@ app.post('/api/calculate', async function (req,res) {
     let maxMarks = subjects[0].obtainedMarks;
     let minSubject = subjects[0].name;
     let maxSubject = subjects[0].name;
-    subjects.map((subject,i)=>{
+    subjects.map((subject, i) => {
       totalMarks += subject.obtainedMarks;
-      if (min > subjects[i].obtainedMarks)
-      {
-          minMarks = subjects[i].obtainedMarks;
-          minSubject = subjects[i].name;
+      if (minMarks > subjects[i].obtainedMarks) {
+        minMarks = subjects[i].obtainedMarks;
+        minSubject = subjects[i].name;
       }
 
-      if (max < subjects[i].obtainedMarks)
-      {
+      if (maxMarks < subjects[i].obtainedMarks) {
         maxMarks = subjects[i].obtainedMarks;
         maxSubject = subjects[i].name;
       }
-     })
-   
-     let percentage = (totalMarks / (subjects.length * 100)) * 100;
+    });
 
-     let marksheetModel = new MarksheetModel();
-     marksheetModel.totalMarks= totalMarks;
-     marksheetModel.subjects = percentage;
-     marksheetModel.minMarks = minMarks;
-     marksheetModel.minSubject = minSubject;
-     marksheetModel.maxMarks = maxMarks;
-     marksheetModel.maxSubject = maxSubject;
-    
+    let percentage = (totalMarks / (subjects.length * 100)) * 100;
+
+    let marksheetModel = new Marksheet();
+    marksheetModel.totalMarks = totalMarks;
+    marksheetModel.percentage = percentage;
+    marksheetModel.subjects = subjects;
+    marksheetModel.minMarks = minMarks;
+    marksheetModel.minSubject = minSubject;
+    marksheetModel.maxMarks = maxMarks;
+    marksheetModel.maxSubject = maxSubject;
+
     const doc = await Marksheet.create(marksheetModel);
-        return res.status(201).json({
-            status:"success",
-            data:{ 
-               doc
-            }
-        })
-    
-  }
-  catch(err){
-    return   res.status(500).json({
+    return res.status(201).json({
+      status: 'success',
+      data: {
+        doc,
+      },
+    });
+  } catch (err) {
+    return res.status(500).json({
       status: 'fail',
-      message: err.message
+      message: err.message,
     });
   }
- 
- });
+});
+
+module.exports = app;
--- frontend/src/components/Form.jsx
+++ frontend/src/components/Form.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Form() {
+
+    // let arr = [4];
+
+
+    let [arr, setArr] = useState([0]);
+
+    console.log("arr length", arr.length)
     return (
         <div className="container">
             <div className="row d-flex justify-content-center py-4">
@@ -12,35 +19,38 @@ export default function Form() {
                             <input placeholder="Enter your name" type="email" className="form-control" aria-describedby="emailHelp" />
                         </div>
                         <div className="form-group">
-                            <select className="custom-select mr-sm-2" id="inlineFormCustomSelect">
-                                <option defaultChecked>Total Subjects</option>
-                                <option value="1">One</option>
-                                <option value="2">Two</option>
-                                <option value="3">Three</option>
+                            <select placeholder="Total Subject" className="custom-select mr-sm-2" id="inlineFormCustomSelect" onChange={(e) => {
+                                console.log(console.log("Selected value", e.target.value))
+
+                                const val = e.target.value;
+
+                                if (!isNaN(val)) {
+
+                                    setArr(Array.from(Array(parseInt(val)).keys()))
+                                }
+
+                                console.log("On change function", e.target.value)
+                            }
+                            }>
+                                <option value={1}>One</option>
+                                <option value={2}>Two</option>
+                                <option value={3}>Three</option>
                             </select>
                         </div>
-                        <button type="submit" className="btn btn-block btn-warning font-weight-bold text-uppercase">Submit</button>
                     </form>
                     {/* Left column closing tag */}
                 </div>
                 {/* Right column */}
                 <div className="col-8">
-                    <form>
-                        <div className="form-group d-flex align-items-center">
-                            <label htmlFor="exampleInputEmail1" className="font-weight-bold">Subject 1:</label>
-                            <input placeholder="Enter subject name" type="email" className="mx-4 form-control" aria-describedby="emailHelp" />
-                            <input placeholder="Marks Obtained" type="number" max="100" className="form-control mr-4" aria-describedby="emailHelp" />
-                        </div>
-                        <div className="form-group d-flex align-items-center">
-                            <label htmlFor="exampleInputEmail1" className="font-weight-bold">Subject 2:</label>
-                            <input placeholder="Enter subject name" type="email" className="mx-4 form-control" aria-describedby="emailHelp" />
-                            <input placeholder="Marks Obtained" type="number" max="100" className="form-control mr-4" aria-describedby="emailHelp" />
-                        </div>
-                        <div className="form-group d-flex align-items-center">
-                            <label htmlFor="exampleInputEmail1" className="font-weight-bold">Subject 3:</label>
-                            <input placeholder="Enter subject name" type="email" className="mx-4 form-control" aria-describedby="emailHelp" />
-                            <input placeholder="Marks Obtained" type="number" max="100" className="form-control mr-4" aria-describedby="emailHelp" />
-                        </div>
+                    <form onSubmit="">
+                        {arr.map((obj, ind) =>
+                            <div className="form-group d-flex align-items-center">
+                                <label htmlFor="exampleInputEmail1" className="font-weight-bold">Subject {ind + 1 + " :"}</label>
+                                <input placeholder="Enter subject name" type="email" className="mx-4 form-control" aria-describedby="emailHelp" />
+                                <input placeholder="Marks Obtained" type="number" max="100" className="form-control mr-4" aria-describedby="emailHelp" />
+                            </div>
+                        )
+                        }
                         <button type="submit" className="btn btn-block btn-warning font-weight-bold text-uppercase">Submit</button>
                     </form>
                 </div>
